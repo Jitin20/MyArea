d1e6d346c4167efaf9176defff99f9e14d273fa1
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-11 14:30:02 +0530

    commit

--- TestMyArea/UnitTest1.cs
+++ TestMyArea/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using MyArea;
 
 namespace TestMyArea
 {
@@ -13,6 +14,26 @@ namespace TestMyArea
         public void Test1()
         {   
             Assert.Pass();
+        }
+        [Test]
+        public  void CircleTest()
+        {
+            //Arrange 
+            Area areas = new Area();
+            float expresult = 75;
+            float actresult = 0;
+            actresult = areas.Circle(5);
+            //Assert
+            Assert.AreEqual(expresult, actresult,5,"Areas not matching");
+         }
+        [Test]
+        public void TriangleTest()
+        {
+            Area area = new Area();
+            float expresult = 12f;
+            float actresult = 0;
+            actresult = area.Triangle(6, 4);
+            Assert.AreEqual(expresult, actresult, "Areas not matching");
         }
     }
 }
