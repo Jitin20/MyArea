e080c7207c756b78bcdeb82ee118156baf0c118e
Jitin20 <jitinsharma2001@rediffmail.com>
2022-08-11 12:19:22 +0530

    Add project files.

---
+++ MyArea/Area.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyArea
+{
+    public class Area
+    {
+        public float Circle(int radius)
+        {
+            return (float)Math.PI * radius * radius;
+        }
+        public float Square(int side)
+        {
+            return (float)side * side;
+        }
+        public float triangle(int b, int h)
+        {
+            return (float).5 * b * h;
+        }
+
+        public int Rectangle(int l, int b)
+        {
+            return l * b;
+        }
+    }
+}
